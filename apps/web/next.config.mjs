d2f7a8e742ee40b5ee35eab029896bf6f4957b64
ThorfinnThor <pixsatmaps@gmail.com>
2026-08-30 17:24:15 +0200

    fix: allow approved card image providers

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -7,7 +7,14 @@ const nextConfig = {
       { protocol: "https", hostname: "cards.scryfall.io" },
       { protocol: "https", hostname: "images.pokemontcg.io" },
       { protocol: "https", hostname: "images.ygoprodeck.com" },
-      { protocol: "https", hostname: "product-images.tcgplayer.com" }
+      { protocol: "https", hostname: "product-images.tcgplayer.com" },
+      { protocol: "https", hostname: "assets.tcgdex.net" },
+      { protocol: "https", hostname: "images.digimoncard.io" },
+      { protocol: "https", hostname: "cards.lorcast.io" },
+      { protocol: "https", hostname: "cdn.swu-db.com" },
+      { protocol: "https", hostname: "storage.googleapis.com" },
+      { protocol: "https", hostname: "legendstory-production-s3-public.s3.amazonaws.com" },
+      { protocol: "https", hostname: "d2wlb52bya4y8z.cloudfront.net" }
     ]
   },
   async headers() {
@@ -20,7 +27,7 @@ const nextConfig = {
           { key: "Permissions-Policy", value: "camera=(), microphone=(), geolocation=()" },
           {
             key: "Content-Security-Policy",
-            value: "img-src 'self' data: https://cards.scryfall.io https://product-images.s3.cardmarket.com https://images.pokemontcg.io https://images.ygoprodeck.com https://product-images.tcgplayer.com"
+            value: "img-src 'self' data: https://cards.scryfall.io https://product-images.s3.cardmarket.com https://images.pokemontcg.io https://images.ygoprodeck.com https://product-images.tcgplayer.com https://assets.tcgdex.net https://images.digimoncard.io https://cards.lorcast.io https://cdn.swu-db.com https://storage.googleapis.com https://legendstory-production-s3-public.s3.amazonaws.com https://d2wlb52bya4y8z.cloudfront.net"
           }
         ]
       }
